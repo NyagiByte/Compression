7837b9ca681455b099e1a6271ab94c6382160ba3
jjfireblast101 <jjfireblast@gmail.com>
2024-01-28 23:12:33 +0000

    Mod Recipes Nuked
    Nuked Mod recipes for easier recipe making.

---
+++ kubejs/server_scripts/Removal/mod.js
@@ -0,0 +1,60 @@
+ServerEvents.recipes(e => {
+//AIOT Botania
+	e.remove({ mod: 'aiotbotania' })
+//Akashic Tome
+	e.remove({ mod: 'akashictome' })
+//Alchemistry
+	e.remove({ mod: 'alchemistry' })
+//Applied Botanistics
+	e.remove({ mod: 'appbot' })
+//Applied Energistics 2
+	e.remove({ mod: 'ae2' })
+//Applied Mekanistics
+	e.remove({ mod: 'appmek' })
+//Bedrock Miner
+	e.remove({ mod: 'bedrockminer' })
+//Biomancy
+	e.remove({ mod: 'biomancy'})
+//Botania
+	e.remove({ mod: 'botania'})
+//Chemlib
+	e.remove({ mod: 'chemlib' })
+//Comapct Machines
+	e.remove({ mod: 'compactmachines' })
+//Compressed Creativity
+	e.remove({ mod: 'compressedcreativity' })
+//Compact Crafting
+	e.remove({ mod: 'compactcrafting' })
+//Chromatic Return
+	e.remove({ mod: 'createchromaticreturn' })
+//Create Chunkloading
+	e.remove({ mod: 'createchunkloading' })
+//Create
+	e.remove({ mod: 'create' })
+//Entangled
+	e.remove({ mod: 'entangled' })
+//Farmer's Delight
+	e.remove({ mod: 'farmersdelight' })
+//Immersive Engineering
+	e.remove({ mod: 'immersiveengineering' })
+//Iron Chests
+	e.remove({ mod: 'ironchests' })
+//Item Filters
+	e.remove({ mod: 'itemfilters' })
+//Mega Cells
+	e.remove({ mod: 'megacells' })
+//Mekanism
+	e.remove({ mod: 'mekanism' })
+//Mythic Botany
+	e.remove({ mod: 'mythicbotany' })
+// Nuclearcraft Neoteric
+	e.remove({ mod: 'nuclearcraft' })
+//Pneumaticcraft Repressurized
+	e.remove({ mod: 'pneumaticcraft' })
+//Quark
+	e.remove({ mod: 'quark' })
+//Tesseract
+	e.remove({ mod: 'tesseract' })
+//Thermal
+	e.remove({ mod: 'thermal' })
+})
