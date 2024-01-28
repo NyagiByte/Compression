bdc076498aae2c75ac2f389afe2430ecd447e98b
Madelyn Byte <nyagiredspice@gmail.com>
2024-01-28 11:59:35 -0500

    nuking vanilla tools

---
+++ kubejs/client_scripts/item_removal.js
@@ -0,0 +1,13 @@
+// priority: 0
+
+JEIEvents.hideItems(event => {
+    event.hide([
+        'minecraft:wooden_shovel', 'minecraft:wooden_sword','minecraft:wooden_pickaxe','minecraft:wooden_axe','minecraft:wooden_hoe',
+		'minecraft:stone_shovel', 'minecraft:stone_sword','minecraft:stone_pickaxe','minecraft:stone_axe','minecraft:stone_hoe',
+		'minecraft:iron_shovel', 'minecraft:iron_sword','minecraft:iron_pickaxe','minecraft:iron_axe','minecraft:iron_hoe',
+		'minecraft:golden_shovel', 'minecraft:golden_sword','minecraft:golden_pickaxe','minecraft:golden_axe','minecraft:golden_hoe',
+		'minecraft:diamond_shovel', 'minecraft:diamond_sword','minecraft:diamond_pickaxe','minecraft:diamond_axe','minecraft:diamond_hoe',
+		'minecraft:netherite_shovel','minecraft:netherite_sword','minecraft:netherite_pickaxe','minecraft:netherite_axe','minecraft:netherite_hoe'
+
+    ])
+})
--- kubejs/server_scripts/vanilla_recipes.js
+++ kubejs/server_scripts/vanilla_recipes.js
@@ -3,6 +3,12 @@ ServerEvents.recipes(event => {
     //------------------------------------------------------------------
     //Removal 
         event.remove({output: 'minecraft:white_wool'});
+        event.remove({output: ['minecraft:wooden_shovel', 'minecraft:wooden_sword','minecraft:wooden_pickaxe','minecraft:wooden_axe','minecraft:wooden_hoe']})
+        event.remove({output: ['minecraft:stone_shovel', 'minecraft:stone_sword','minecraft:stone_pickaxe','minecraft:stone_axe','minecraft:stone_hoe']})
+        event.remove({output: ['minecraft:iron_shovel', 'minecraft:iron_sword','minecraft:iron_pickaxe','minecraft:iron_axe','minecraft:iron_hoe']})
+        event.remove({output: ['minecraft:golden_shovel', 'minecraft:golden_sword','minecraft:golden_pickaxe','minecraft:golden_axe','minecraft:golden_hoe']})
+        event.remove({output: ['minecraft:diamond_shovel', 'minecraft:diamond_sword','minecraft:diamond_pickaxe','minecraft:diamond_axe','minecraft:diamond_hoe']})
+        event.remove({output: ['minecraft:netherite_shovel','minecraft:netherite_sword','minecraft:netherite_pickaxe','minecraft:netherite_axe','minecraft:netherite_hoe']})
     //------------------------------------------------------------------
     //Vanilla Items Shapeless
         //Paper
