24acb58fa5570d06e45eccc19c81908672c2ce4d
jjfireblast101 <jjfireblast@gmail.com>
2024-01-31 22:44:06 +0000

    Added Compact Crafting & PonderJS Mods
    Some more tweaks to format, but mostly to add Compact Crafting and PonderJS.

--- kubejs/server_scripts/Removal/create.js
+++ kubejs/server_scripts/Removal/create.js
@@ -1,9 +1,10 @@
 ServerEvents.recipes(e => {
 //------------------------------------------------------------------
 	//Tools
-    	
+    	e.remove({output:'create:wrench'})
     //Items
-    	
+    	e.remove({output:'create:rose_quartz'})
+        e.remove({output:'create:whisk'})
     //Blocks
     	e.remove({output:'create:shaft'})
         e.remove({output:'create:cogwheel'})
@@ -12,7 +13,6 @@ ServerEvents.recipes(e => {
         e.remove({output:'create:basin'})
         e.remove({output:'create:depot'})
         e.remove({output:'create:mechanical_press'})
-        e.remove({output:'create:whisk'})
         e.remove({output:'create:belt_connector'})
         e.remove({output:'create:mechanical_drill'})
         e.remove({output:'create:mechanical_saw'})
@@ -22,7 +22,6 @@ ServerEvents.recipes(e => {
         e.remove({output:'create:empty_blaze_burner'})
         e.remove({output:'create:brass_hand'})
         e.remove({output:'create:hand_crank'})
-        e.remove({output:'create:rose_quartz'})
         e.remove({output:'create:mechanical_crafter'})
         e.remove({output:'create:chute'})
         e.remove({output:'create:andesite_casing'})
