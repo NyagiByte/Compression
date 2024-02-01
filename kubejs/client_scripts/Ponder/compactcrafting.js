b11a88df4d6774601da5017a68929c2f8bcf692a
jjfireblast101 <jjfireblast@gmail.com>
2024-02-01 23:17:10 +0000

    More Ponder Stoof
    YAY

---
+++ kubejs/client_scripts/Ponder/compactcrafting.js
@@ -0,0 +1,14 @@
+Ponder.registry((e) => {
+//------------------------------------------------------------------
+    //Glass
+        e.create("compactcrafting:field_projector").scene("Glass", "Blasted Sand", (scene, util) => {
+            scene.showBasePlate();
+            scene.idle(5);
+            scene.world.setBlock([2,1,2], "minecraft:glass", false);
+            scene.world.showSection([2,1,2], Facing.down);
+            scene.idle(10);
+            scene.text(40, "You can see through this...", [2.5,1,2.5]);
+            scene.idle(20);
+        });
+//------------------------------------------------------------------
+});
