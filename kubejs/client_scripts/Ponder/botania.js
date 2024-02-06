c3a924507ddea0453b824ea3dfaebdd466defff9
jjfireblast101 <jjfireblast@gmail.com>
2024-02-06 14:03:31 +0000

    Ponder Stuff
    IDK

--- kubejs/client_scripts/Ponder/botania.js
+++ kubejs/client_scripts/Ponder/botania.js
@@ -6,11 +6,11 @@ Ponder.registry((e) => {
         e.create("botania:apothecary_default")
             .scene("Apothecary", "Crafting Using Apothecary", "kubejs:botania_5x5", (scene, util) => {
                 scene.showBasePlate();
-                scene.idle(5);
+                scene.idle(10);
                 scene.world.setBlock([2,1,2], "botania:apothecary_default", false);
                 scene.world.showSection([2,1,2], Facing.down);
                 scene.idle(20);
-                scene.overlay.showOutline(PonderPalette.GREEN, "airgap", [2,1,2], 40);
+                scene.overlay.showOutline(PonderPalette.GREEN, "airgap", [2,1,2], 60);
                 scene.text(60, "This Is A Petal Apothecary, It Is Heavily Used In Botania For Creating Flowers", [2,1.5,2]);
                 scene.idle(70);
                 scene.text(60, "As An Example Let's Make A Pure Daisy", [2,1.5,2])
@@ -19,7 +19,7 @@ Ponder.registry((e) => {
                 scene.showControls(40, [2.5, 2.5, 2.5], "down")
                     .rightClick()
                     .withItem("minecraft:water_bucket");
-                scene.idle(60);
+                scene.idle(50);
                 scene.particles.simple(5, "splash", [2.5,2.5,2.5]).density(5);
                 scene.world.modifyBlock([2,1,2], () => Block.id("botania:apothecary_default").with("fluid", "water"), true);
                 scene.idle(40);
@@ -57,11 +57,36 @@ Ponder.registry((e) => {
                 scene.world.modifyEntity(seed, (r) => {r.discard()});
                 const daisy = scene.world.createItemEntity([2.5,2,2.5], [-0.1,0.5,-0.1], "botania:pure_daisy");
                 scene.world.modifyBlock([2,1,2], () => Block.id("botania:apothecary_default").with("fluid", "empty"), true);
+                scene.markAsFinished()
             });
     //Pure Daisy
         e.create("botania:pure_daisy")
-            .scene("Pure Daisy", "Utilising The Pure Daisy", (scene, util) => {
-                scene.showBasePlate()
-            })
-//------------------------------------------------------------------
+            .scene("PureDaisy", "Utilising The Pure Daisy", "kubejs:botania_7x7", (scene, util) => {
+                scene.showBasePlate();
+                scene.idle(10);
+                scene.world.setBlock([3,1,1], "botania:pure_daisy", false);
+                scene.world.showSection([3,1,3], Facing.down);
+                scene.idle(20);
+                scene.overlay.showOutline(PonderPalette.WHITE, "airgap", [3,1,3], 60);
+                scene.text(60, "This Is A Pure Daisy It Is A Functional Flower That Changes Blocks Placed Around It", [2,1.5,2]);
+                scene.idle(70);
+                scene.world.setBlock([2,1,2], "minecraft:stone",true);
+                scene.idle(5);
+                scene.world.setBlock([3,1,2], "minecraft:stone",true);
+                scene.idle(5);
+                scene.world.setBlock([4,1,2], "minecraft:stone",true);
+                scene.idle(5);
+                scene.world.setBlock([4,1,3], "minecraft:stone",true);
+                scene.idle(5);
+                scene.world.setBlock([4,1,4], "minecraft:stone",true);
+                scene.idle(5);
+                scene.world.setBlock([3,1,4], "minecraft:stone",true);
+                scene.idle(5);
+                scene.world.setBlock([2,1,4], "minecraft:stone",true);
+                scene.idle(5);
+                scene.world.setBlock([2,1,3], "minecraft:stone",true);
+                scene.idle(5);
+                scene.markAsFinished()
+            });
 });
+//------------------------------------------------------------------
