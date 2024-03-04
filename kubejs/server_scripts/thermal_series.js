92960a45c24e4cf2861c2681240cd662e75671b4
jjfireblast101 <jjfireblast@gmail.com>
2024-03-04 21:01:02 +0000

    Some Create Stoofs
    Yes

--- kubejs/server_scripts/thermal_series.js
+++ kubejs/server_scripts/thermal_series.js
@@ -1,16 +1,6 @@
 ServerEvents.recipes(e => {
 //------------------------------------------------------------------
     //Shaped Crafting
-        //Drill
-            e.shaped('thermal:drill_head', 
-                [ 
-                    ' AA', 
-                    'ABA',
-                    'BA '  
-                ], {
-                        A: 'create:iron_sheet',
-                        B: 'create:shaft'
-                }).id('compression:compression/thermal/shaped/drill_head')
         //Sawblade
             e.shaped('thermal:saw_blade', 
                 [ 
