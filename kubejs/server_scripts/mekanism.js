a447e15957d28a4a14fdd794e6da29798b679003
Madelyn Byte <nyagiredspice@gmail.com>
2024-03-06 01:53:43 -0500

    Mekanism recipes moved to datapacks from kubejs
    Only 1 recipe - charcoal dust

--- kubejs/server_scripts/mekanism.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.recipes(e => {
-//------------------------------------------------------------------
-    //Shaped
-    //Shapeless
-//------------------------------------------------------------------
-    //Milling
-        //Charcoal Dust
-            e.recipes.create.milling(['mekanism:dust_charcoal'], ['minecraft:charcoal']).id('compression:compression/mekanism/milling/dust_charcoal')
-    })
