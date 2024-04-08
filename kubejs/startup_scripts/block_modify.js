0f214ed6e8ed3862d7b3084468f7682ef6681c7a
Madelyn Byte <nyagiredspice@gmail.com>
2024-04-08 00:32:12 -0400

    block modify on petal apothacary

--- kubejs/startup_scripts/block_modify.js
+++ kubejs/startup_scripts/block_modify.js
@@ -2,4 +2,7 @@ BlockEvents.modification(e => {
     e.modify('botania:livingrock', block => {
       block.requiresTool = false
     })
+    e.modify('botania:apothecary_default', block => {
+      block.requiresTool = false
+    })
   })
