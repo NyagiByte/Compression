600ca646b2e865aa12b421ed2dd8646148e10f54
jjfireblast101 <jjfireblast@gmail.com>
2024-04-28 18:00:58 +0100

    Fixing Seeds Attempt 1
    Seeds are great aren't they...

--- kubejs/server_scripts/loottables.js
+++
@@ -1,7 +0,0 @@
-LootJS.modifiers((event) => {
-    event.addLootTableModifier("minecraft:blocks/grass").randomChance(0.05).addLoot('minecraft:pumpkin_seeds')
-    event.addLootTableModifier("minecraft:blocks/grass").randomChance(0.05).addLoot('minecraft:melon_seeds')
-    event.addLootTableModifier("minecraft:blocks/grass").randomChance(0.05).addLoot('minecraft:beetroot_seeds')
-    event.addLootTableModifier("minecraft:blocks/grass").randomChance(0.05).addLoot('farmersdelight:cabbage_seeds')
-    event.addLootTableModifier("minecraft:blocks/grass").randomChance(0.05).addLoot('farmersdelight:tomato_seeds')
-})
