26cc54be49c21bf33a4c95cc602256370e42fe57
Madelyn Byte <nyagiredspice@gmail.com>
2024-09-10 04:16:35 -0500

    sodium output trim

---
+++ kubejs/server_scripts/chemlib_fusion_removals.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(e => {
+
+    //I'd rather shove a light bulb up my ass and shatter it than remove these one by one with the .mcmeta
+    e.remove({ output: 'chemlib:sodium', type: 'alchemistry:fusion' })
+
+})
