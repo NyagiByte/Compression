2f609a6809fc9a31337322a160e8e1fbcb88905d
Madelyn Byte <nyagiredspice@gmail.com>
2024-09-10 04:20:37 -0500

    fusion recipe removal rework

--- kubejs/server_scripts/chemlib_fusion_removals.js
+++ kubejs/server_scripts/chemlib_fusion_removals.js
@@ -1,6 +1,15 @@
 ServerEvents.recipes(e => {
 
     //I'd rather shove a light bulb up my ass and shatter it than remove these one by one with the .mcmeta
+    e.remove({ output: 'chemlib:helium', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:lithium', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:beryllium', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:boron', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:carbon', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:nitrogen', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:oxygen', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:fluorine', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:neon', type: 'alchemistry:fusion' })
     e.remove({ output: 'chemlib:sodium', type: 'alchemistry:fusion' })
 
 })
