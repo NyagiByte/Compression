ca9eb92880fe788ec835221afa006e33213a3206
Madelyn Byte <nyagiredspice@gmail.com>
2024-09-11 02:01:36 -0500

    krypton, rubidium, strontium output trim

--- kubejs/server_scripts/chemlib_fusion_removals.js
+++ kubejs/server_scripts/chemlib_fusion_removals.js
@@ -35,5 +35,8 @@ ServerEvents.recipes(e => {
     e.remove({ output: 'chemlib:arsenic', type: 'alchemistry:fusion' })
     e.remove({ output: 'chemlib:selenium', type: 'alchemistry:fusion' })
     e.remove({ output: 'chemlib:bromine', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:krypton', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:rubidium', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:strontium', type: 'alchemistry:fusion' })
 
 })
