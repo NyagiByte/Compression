caa9c8d1c886f78114735185557bb44ac6b1a5b0
Madelyn Byte <nyagiredspice@gmail.com>
2024-09-11 01:46:15 -0500

    selenium output trim

--- kubejs/server_scripts/chemlib_fusion_removals.js
+++ kubejs/server_scripts/chemlib_fusion_removals.js
@@ -33,5 +33,6 @@ ServerEvents.recipes(e => {
     e.remove({ output: 'chemlib:gallium', type: 'alchemistry:fusion' })
     e.remove({ output: 'chemlib:germanium', type: 'alchemistry:fusion' })
     e.remove({ output: 'chemlib:arsenic', type: 'alchemistry:fusion' })
+    e.remove({ output: 'chemlib:selenium', type: 'alchemistry:fusion' })
 
 })
