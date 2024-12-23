329b85fa720b91c48d643b00f676d645457336e8
Big Barza <francesco.b99@hotmail.it>
2024-12-23 22:34:46 +0100

    Tools attempt 2: Manasteel Boogaloo (Switch things up a bit in tool_tiers.js so that the game starts) Tier level seems to work, at least for livingrock. Durability does not. Reasons unknown, as per usual with anything kubejs,

--- kubejs/startup_scripts/tool_tiers.js
+++ kubejs/startup_scripts/tool_tiers.js
@@ -1,71 +1,85 @@
-ItemEvents.toolTierRegistry(event => {
-    event.add('livingrock', livingrock = tier => {
-        tier.uses = 250
-        tier.speed = 6.0
-        tier.attackDamageBonus = 2.0
-        tier.level = 2
-        tier.enchantmentValue = 14
-        tier.repairIngredient = 'botania:livingrock'
-    })
-    event.add('manasteel', manasteel = tier => {
-        tier.uses = 1521
-        tier.speed = 8.0
-        tier.attackDamageBonus = 3.0
-        tier.level = 3
-        tier.enchantmentValue = 10
-        tier.repairIngredient = '#forge:ingots/manasteel'
-    })
-    event.add('elementium', elementium = tier => {
-        tier.uses = 2031
-        tier.speed = 9.0
-        tier.attackDamageBonus = 4.0
-        tier.level = 4
-        tier.enchantmentValue = 15
-        tier.repairIngredient = '#forge:ingots/elementium'
-    })
-    event.add('terrasteel', terrasteel = tier => {
-        tier.uses = 3473
-        tier.speed = 10.0
-        tier.attackDamageBonus = 5.0
-        tier.level = 5
-        tier.enchantmentValue = 16
-        tier.repairIngredient = '#forge:ingots/terrasteel'
-    })
-    event.add('alfsteel', alfsteel = tier => {
-        tier.uses = 5127
-        tier.speed = 11.0
-        tier.attackDamageBonus = 6.0
-        tier.level = 6
-        tier.enchantmentValue = 17
-        tier.repairIngredient = 'mythicbotany:alfsteel_ingot'
-    })
-    event.add('bedrock', bedrock = tier => {
-        tier.uses = 7398 // not so unbreakable huh
-        tier.speed = 12.0
-        tier.attackDamageBonus = 7.0
-        tier.level = 7
-        tier.enchantmentValue = 18
-        tier.repairIngredient = 'minecraft:bedrock'
-    })
-  })
+const livingrock = tier => {
+    tier.uses = 250 //This isn't doing anything
+    tier.speed = 6.0
+    tier.attackDamageBonus = 2.0
+    tier.level = 2 //This however, is. Why? Great Question(tm)
+    tier.enchantmentValue = 14
+    tier.repairIngredient = 'botania:livingrock'
+}
 
+const manasteel = tier => {
+    tier.uses = 1521
+    tier.speed = 8.0
+    tier.attackDamageBonus = 3.0
+    tier.level = 3
+    tier.enchantmentValue = 10
+    tier.repairIngredient = '#forge:ingots/manasteel'
+}
+
+const elementium = tier => {
+    tier.uses = 2031
+    tier.speed = 9.0
+    tier.attackDamageBonus = 4.0
+    tier.level = 4
+    tier.enchantmentValue = 15
+    tier.repairIngredient = '#forge:ingots/elementium'
+}
+
+const terrasteel = tier => {
+    tier.uses = 3473
+    tier.speed = 10.0
+    tier.attackDamageBonus = 5.0
+    tier.level = 5
+    tier.enchantmentValue = 16
+    tier.repairIngredient = '#forge:ingots/terrasteel'
+}
+
+const alfsteel = tier => {
+    tier.uses = 5127
+    tier.speed = 11.0
+    tier.attackDamageBonus = 6.0
+    tier.level = 6
+    tier.enchantmentValue = 17
+    tier.repairIngredient = 'mythicbotany:alfsteel_ingot'
+}
+
+const bedrock = tier => {
+    tier.uses = 7398 // not so unbreakable huh
+    tier.speed = 12.0
+    tier.attackDamageBonus = 7.0
+    tier.level = 7
+    tier.enchantmentValue = 18
+    tier.repairIngredient = 'minecraft:bedrock'
+}
+
+//Does this do anything?
+ItemEvents.toolTierRegistry(event => {
+event.add('livingrock', livingrock)
+event.add('manasteel', manasteel)
+event.add('elementium', elementium)
+event.add('terrasteel', terrasteel)
+event.add('alfsteel', alfsteel)
+event.add('bedrock', bedrock)
+})
+
+//Saw a thread saying item.tier didn't work. So i went for setTier instead.
 ItemEvents.modification(event => {
-    event.modify('aiotbotania:livingrock_sword', item => {
-        item.tier = livingrock
-    })
-    event.modify('aiotbotania:livingrock_axe', item => {
-        item.tier = livingrock
-    })
-    event.modify('aiotbotania:livingrock_pickaxe', item => {
-        item.tier = livingrock
-    })
-    event.modify('aiotbotania:livingrock_hoe', item => {
-        item.tier = livingrock
-    })
-    event.modify('aiotbotania:livingrock_shovel', item => {
-        item.tier = livingrock
-    })
-    event.modify('aiotbotania:livingrock_aiot', item => {
-        item.tier = livingrock
-    })
+event.modify('aiotbotania:livingrock_sword', item => {
+    item.setTier(livingrock)
+})
+event.modify('aiotbotania:livingrock_axe', item => {
+    item.setTier(livingrock)
+})
+event.modify('aiotbotania:livingrock_pickaxe', item => {
+    item.setTier(livingrock)
+})
+event.modify('aiotbotania:livingrock_hoe', item => {
+    item.setTier(livingrock)
+})
+event.modify('aiotbotania:livingrock_shovel', item => {
+    item.setTier(livingrock)
+})
+event.modify('aiotbotania:livingrock_aiot', item => {
+    item.setTier(livingrock)
+})
 })
