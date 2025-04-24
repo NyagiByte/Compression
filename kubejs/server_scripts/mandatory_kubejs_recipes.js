89886982b70e73866c21de480e3eef5a23dafc0d
Madelyn Byte <nyagiredspice@gmail.com>
2025-04-23 19:53:55 -0500

    Nb&b Chemical updating

---
+++ kubejs/server_scripts/mandatory_kubejs_recipes.js
@@ -0,0 +1,21 @@
+ServerEvents.recipes(event => {
+    //This file is for recipes that have specific requirements like:
+    //Consume/Not Consume item
+    //1.19.2 datapacks are unable to support this function
+
+    event.shapeless('1x nyagibits_bytes:bucket_of_salt_water', [
+        'minecraft:water_bucket',
+        'mekanism:salt'
+    ]).replaceIngredient("minecraft:water_bucket", "minecraft:air"),
+
+        'ABC', 
+        ' D ' 
+      ], {
+        A: 'create:copper_nugget', 
+        B: '#forge:torn_fabric',
+        C: 'create:zinc_nugget',
+        D: 'nyagibits_bytes:bucket_of_salt_water'   
+      }
+    ).replaceIngredient("nyagibits_bytes:bucket_of_salt_water", "minecraft:air")
+
+})
