bccfeb6ed1732a39b453477f0ed7dc48eed151d2
Asperger <m4n0123456789@gmail.com>
2025-05-21 14:34:01 -0300

    Making better ponder

--- kubejs/client_scripts/Ponder/minecraft.js
+++ kubejs/client_scripts/Ponder/minecraft.js
@@ -26,13 +26,14 @@ Ponder.registry((e) => {
                 scene.showBasePlate();    //this shows the baseplate selected
                 scene.idle(20);
                 scene.text(70, "This prison seems to work similarly to the 'Garden of Glass'...");
-				scene.idle(80);
-				scene.text(60, "...enabling you to harvest pebbles from the soil beneath your feet.", [2.5,1,2.5]); //text that appears pointing towards the cords
-				scene.idle(50);
+                scene.idle(80);
+    			scene.text(60, "...enabling you to harvest pebbles from the soil beneath your feet.", [2.5,1,2.5]); //text that appears pointing towards the cords
+    			scene.idle(50);
+    			scene.addKeyframe();
                 scene.showControls(40, [2.5,1,2.5], "down")                 //setting the instruction of clicking
                     .rightClick()
                     .whileSneaking();
-				scene.idle(50);
+    		    scene.idle(50);
                 var pebbles = [];     //hippity hoppity barza's code is now my property
                     pebbles.push(scene.world.createItemEntity([2.5,1,2.5], [0,0.4,0], "botania:pebble"))
                         for(let i = 0; i<8;i++){
@@ -41,8 +42,10 @@ Ponder.registry((e) => {
                                 (Math.random())/2,
                                 (Math.random()/3)-0.16
                             ], "botania:pebble"));
-							scene.idle(10);                   //delay per item spawn
+    			        scene.idle(10);                   //delay per item spawn
                         }
+    			scene.text(60, "With these pebbles, you can craft cobblestone.");
+    			scene.idle(20);
                 scene.markAsFinished();
-        });
+            });
 });
