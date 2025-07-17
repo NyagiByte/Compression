207e9703ca04e7fae16bc552c5ace61c79cea457
Big Barza <francesco.b99@hotmail.it>
2025-07-17 16:45:01 +0200

    Make IE assembly take only full acetylene tanks

--- kubejs/server_scripts/mandatory_kubejs_recipes.js
+++ kubejs/server_scripts/mandatory_kubejs_recipes.js
@@ -20,6 +20,24 @@ ServerEvents.recipes(event => {
       }
     ).replaceIngredient("nyagibits_bytes:bucket_of_salt_water", "minecraft:air"),*/
 
+    event.shaped('nyagibits_bytes:immersive_assembly', 
+      [
+        'ABC',
+        'DEF',
+        'GHI'
+      ],
+      {
+        A: 'nyagibits_bytes:triple_bay_4s_series_toolbox',
+        B: 'nyagibits_bytes:duct_tape',
+        C: 'nyagibits_bytes:wd_40',
+        D: 'nyagibits_bytes:high_strength_concrete',
+        E: 'create:brass_casing',
+        F: 'nyagibits_bytes:osha_approved_gear',
+        G: Item.of('nyagibits_bytes:acetylene_tank', '{Damage:0}'),
+        H: 'nyagibits_bytes:crate_of_industrial_components',
+        I: 'nyagibits_bytes:high_temperature_heat_exchanger'
+      }).replaceIngredient('nyagibits_bytes:acetylene_tank', 'minecraft:air')
+
     event.shapeless(Item.of('botania:lexicon', '{"botania:elven_unlock":1b}'), [
       'botania:lexicon',
       'botania:corporea_spark'
